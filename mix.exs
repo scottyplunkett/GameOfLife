489bcfa81800022268bcf3ee3ac0eb8aa68bae16
scottyplunkett <splunkett@8thLight.com>
2017-11-15 13:44:01 -0600

    removed extra comma from mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GameOfLife.Mixfile do
       app: :game_of_life,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env == :prod
     ]
   end
 end
