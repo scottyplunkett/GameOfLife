73f27216fb82a26cb50b847bd0e68ec20768623f
scottyplunkett <splunkett@8thLight.com>
2017-11-15 11:34:23 -0600

    Removed Dependency And Extra Applications Logger Functions

--- mix.exs
+++ mix.exs
@@ -6,21 +6,7 @@ defmodule GameOfLife.Mixfile do
       app: :game_of_life,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
-    ]
-  end
-
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
+      start_permanent: Mix.env == :prod
     ]
   end
 end
