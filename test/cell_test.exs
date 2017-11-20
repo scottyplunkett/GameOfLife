225252a8ca3827888a012e58e972e92db7d1d4a4
scottyplunkett <splunkett@8thLight.com>
2017-11-20 11:35:06 -0600

    Set up neighborhood with functions for getting all neighbors & counting live neighbors

--- test/cell_test.exs
+++ test/cell_test.exs
@@ -1,7 +1,7 @@
 defmodule GameOfLife.CellTest do
   use ExUnit.Case, async: true
 
-  setup _context do 
+  setup _cell do 
     location = {1,1}
     {:ok, [
             location: location,
@@ -11,21 +11,27 @@ defmodule GameOfLife.CellTest do
     }
   end 
 
-  test "A Cell can stay or come alive", context do 
-    assert GameOfLife.Cell.live(context[:dead],context[:location]) == %{{1,1} => :alive}
-    assert GameOfLife.Cell.live(context[:living],context[:location]) == %{{1,1} => :alive}
+  test "A Cell can stay or come alive", cell do 
+    assert GameOfLife.Cell.live(cell[:dead],cell[:location]) == %{{1,1} => :alive}
+    assert GameOfLife.Cell.live(cell[:living],cell[:location]) == %{{1,1} => :alive}
   end 
 
-  test "A Cell can stay or be dead", context do 
-    assert GameOfLife.Cell.die(context[:dead],context[:location]) == %{{1,1} => :dead}
-    assert GameOfLife.Cell.die(context[:living],context[:location]) == %{{1,1} => :dead}
+  test "A Cell can stay or be dead", cell do 
+    assert GameOfLife.Cell.die(cell[:dead],cell[:location]) == %{{1,1} => :dead}
+    assert GameOfLife.Cell.die(cell[:living],cell[:location]) == %{{1,1} => :dead}
   end
 
-  test "A living Cell responds true to is_alive?", context do 
-    assert GameOfLife.Cell.is_alive?(context[:living],context[:location]) == true
+  test "A living Cell responds true to is_alive", cell do 
+    assert GameOfLife.Cell.is_alive(cell[:living],cell[:location]) == true
   end
 
-  test "A dead Cell responds false to is_alive?", context do 
-    assert GameOfLife.Cell.is_alive?(context[:dead],context[:location]) == false
+  test "A dead Cell responds false to is_alive", cell do 
+    assert GameOfLife.Cell.is_alive(cell[:dead],cell[:location]) == false
   end
+
+  test "A cell can find the location of his neighbor" do
+    assert GameOfLife.Cell.find_neighbor_locations({0,0}) == [{1,0}, {1,1}, {0,1}, {-1,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}]
+    assert GameOfLife.Cell.find_neighbor_locations({3,5}) == [{4,5}, {4,6}, {3,6}, {2,6}, {2,5}, {2,4}, {3,4}, {4,4}]
+  end
+
 end
---
+++ test/neighborhood_test.exs
@@ -0,0 +1,25 @@
+defmodule GameOfLife.NeighborhoodTest do
+  use ExUnit.Case, async: true
+
+  setup _the_list do
+    {:ok, [
+            of_two_living_cells: [{1,1},{1,2}],
+            of_three_living_cells: [{1,1},{1,2},{2,2}],
+            of_four_living_cells: [{1,1},{1,2},{2,2},{2,0}],
+            of_neighbors_of_two_living_cells:  [{2, 1}, {2, 2}, {1, 2}, {0, 2}, {0, 1}, {0, 0}, {1, 0}, {2, 0}, {2, 2}, {2, 3}, {1, 3}, {0, 3}, {0, 2}, {0, 1}, {1, 1}, {2, 1}]
+          ]
+    }
+  end
+
+  test "A neighborhood can gather the nieghbors of a the_list of cells", the_list do 
+    assert GameOfLife.Neighborhood.get_all_neighbors_of(the_list[:of_two_living_cells]) == the_list[:of_neighbors_of_two_living_cells] 
+  end
+
+  test "A neighborhood can count number of neighbors of each live cell that are also live cells", the_list do 
+    assert GameOfLife.Neighborhood.count_live_neighbors_of(the_list[:of_two_living_cells]) == [[{1,1},1],[{1,2},1]]
+    assert GameOfLife.Neighborhood.count_live_neighbors_of(the_list[:of_three_living_cells]) == [[{1,1},2],[{1,2},2],[{2,2},2]]
+    assert GameOfLife.Neighborhood.count_live_neighbors_of(the_list[:of_four_living_cells]) == [[{1,1},3],[{1,2},2],[{2,2},2],[{2,0},1]]
+  end
+
+end
+
