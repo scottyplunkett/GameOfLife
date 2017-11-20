6d826c5cb57aef27ef63a7101a360c231799ead0
scottyplunkett <splunkett@8thLight.com>
2017-11-20 16:19:09 -0600

    first pass on under/overpop. & reproduction

--- lib/generation.ex
+++ lib/generation.ex
@@ -1,18 +1,55 @@
 defmodule GameOfLife.Generation do
 
- @doc """
+@doc """
   Underpopulation occurs thusly: Any live cell with fewer than two live neighbours dies, as if caused by under-population.
     Steps-
     1. Get all live cells
     2. Get all their neighbors
     3. Count the neighbors that are also a live cell
-        if live_neighbors < 2    #=> put into next_gen on list
- """ 
+    4. Where live_neighbors >= 2  #=> pass to next generation
+""" 
   def kill_underpopulated_from(current_generation) do
     GameOfLife.Neighborhood.count_live_neighbors_of(current_generation) |>
-    Enum.filter(
-      fn([{_x,_y},live_neighbor_count]) -> 
-        live_neighbor_count >= 2 
-      end)
+      Enum.filter(
+        fn([{_x,_y},live_neighbor_count]) -> 
+          live_neighbor_count >= 2 
+        end)
   end
+
+@doc """
+  Overpopulation occurs thusly: Any live cell with more than three live neighbours dies, as if by over-population.
+    Steps-
+    1. Get all live cells
+    2. Get all their neighbors
+    3. Count the neighbors that are also a live cell
+    4. Where live_neighbors <= 3  #=> pass to next generation
+"""
+  def kill_overpopulated_from(current_generation) do 
+    GameOfLife.Neighborhood.count_live_neighbors_of(current_generation) |>
+      Enum.filter(
+        fn([{_x,_y},live_neighbor_count]) -> 
+          live_neighbor_count <= 3 
+        end)
+  end
+
+@doc """
+  Reproduction occurs thusly: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    Steps-
+    1. Get all live cells
+    2. Get all their neighbors
+    3. Map occurences of each neighbor to their respective locations
+      (ex. %{{dead_1x,dead_1y} => times_dead1_occurs, {dead_2x,dead_2y} => times_dead2_occurs)
+    4. If a dead cell occurs three times 
+      (ex. %{{dead_x,dead_y} => 3}) #=> pass to next generation
+"""
+  def reproduce_from(current_generation) do 
+    locations_with_occurences = current_generation
+    |> GameOfLife.Neighborhood.get_all_neighbors_of() 
+    |> Enum.reduce(%{}, 
+        fn(cell, occurs) -> 
+          Map.update(occurs, cell, 1, &(&1 + 1)) 
+        end)
+    for {location,occurs} when occurs === 3 <- locations_with_occurences, do: location 
+  end 
+
 end
--- test/generation_test.exs
+++ test/generation_test.exs
@@ -3,8 +3,22 @@ defmodule GameOfLife.GenerationTest do
 
   setup _present_generation do
     {:ok, [
-            with_three_cells_not_underpopulated: [{1,1},{1,2},{2,2},{2,0}],
-            not_underpopulated:   [[{1,1},3],[{1,2},2],[{2,2},2]]
+            with_three_cells_not_underpopulated: 
+              [{1,1},{1,2},{2,2},{2,0}],
+            not_underpopulated:
+              [[{1,1},3],[{1,2},2],[{2,2},2]],
+            with_one_cell_overpopulated: 
+              [{1,1},{2,0},{2,1},{2,2},{3,1}],
+            without_one_overpopulated: 
+              [[{1,1},3],[{2,0},3],[{2,2},3],[{3,1},3]],
+            with_two_cells_overpopulated: 
+              [{1,1},{2,0},{2,1},{2,2},{3,1},{-2,-1},{-2,-2},{-2,-3},{-1,-2},{-3,-2}],
+            without_two_overpopulated: 
+              [[{1,1},3],[{2,0},3],[{2,2},3],[{3,1},3],[{-2,-1},3],[{-2,-3},3],[{-1,-2},3],[{-3,-2},3]],
+            with_three_living_cells_around_one_dead_cell:
+              [{4,2},{6,2},{5,3}],
+            following_reproduction:
+              [{5,2}]
           ]
     }
   end
@@ -12,4 +26,14 @@ defmodule GameOfLife.GenerationTest do
   test "A generation can kill off underpopulated cells", present_generation do 
     assert GameOfLife.Generation.kill_underpopulated_from(present_generation[:with_three_cells_not_underpopulated]) == present_generation[:not_underpopulated] 
   end
+
+  test "A generation can kill off overpopulated cells", present_generation do 
+    assert GameOfLife.Generation.kill_overpopulated_from(present_generation[:with_one_cell_overpopulated]) == present_generation[:without_one_overpopulated]
+    assert GameOfLife.Generation.kill_overpopulated_from(present_generation[:with_two_cells_overpopulated]) == present_generation[:without_two_overpopulated]  
+  end
+
+  test "A generation can reproduce in dead cell locations where there are exactly 3 living neighbors", present_generation do 
+      assert GameOfLife.Generation.reproduce_from(present_generation[:with_three_living_cells_around_one_dead_cell]) == present_generation[:following_reproduction]
+  end
+
 end
