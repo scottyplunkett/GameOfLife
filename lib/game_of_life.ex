07051e8425c7ef3c9d53b2375da91a35194b4763
scottyplunkett <splunkett@8thLight.com>
2017-11-25 20:58:46 -0600

    GOL with finite world of 9 cells

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,2 +1,82 @@
 defmodule GameOfLife do
+  def lives?(status,living_neighbor_count) do 
+    if status == :alive do 
+      living_neighbor_count >= 2 && living_neighbor_count <= 3
+    else 
+      living_neighbor_count === 3 
+    end
+  end 
+
+  def northeast({col,row}) do 
+    if row === 1 do 
+      if col === 1, do: {-2,-2}, else: {1,-2}
+    else 
+      if col === 1, do: {-2,1}, else: {1,1}
+    end
+  end
+
+  def southeast({col,row}) do 
+    if row === -1 do 
+      if col === 1, do: {-2,2}, else: {1,2}
+    else 
+      if col === 1, do: {-2,-1}, else: {1,-1}
+    end
+  end
+  
+  def northwest({col,row}) do 
+    if row === 1 do 
+      if col === -1, do: {2,-2}, else: {-1,-2}
+    else 
+      if col === -1, do: {2, 1}, else: {-1,1}
+    end
+  end
+  
+  def southwest({col,row}) do 
+    if row === -1 do 
+      if col === -1, do: {2,2}, else: {-1,2}
+    else 
+      if col === -1, do: {2,-1}, else: {-1,-1}
+    end
+  end
+
+  def living_neighbors(world,{col,row}) do
+    n  = if row   ==  1,  do: {0,-2}, else: {0,1} 
+    s  = if row   == -1,  do: {0,2},  else: {0,-1}
+    w  = if col == -1,  do: {2,0},  else: {-1,0}
+    e  = if col ==  1,  do: {-2,0}, else: {1,0}
+    ne = {col,row} |> northeast
+    se = {col,row} |> southeast
+    nw = {col,row} |> northwest
+    sw = {col,row} |> southwest
+    directions = [
+                  nw,n,ne,
+                   w,   e,
+                  sw,s,se
+                 ]
+    neighbors = for {distance_h, distance_v} <- directions, do: 
+      world[{col+distance_h,row+distance_v}]
+    Enum.count(neighbors, fn(status) -> status == :alive end)
+  end
+
+  def tick(world,generations) do
+    :timer.sleep 100
+    if generations == 0 do 
+      IO.inspect world 
+    else
+      IO.puts "\n"
+      IO.puts "==============================="
+      IO.puts "#{world[{-1,1}]}---#{world[{0,1}]}---#{world[{1,1}]}"
+      IO.puts "#{world[{-1,0}]}---#{world[{0,0}]}---#{world[{1,0}]}"
+      IO.puts "#{world[{-1,-1}]}---#{world[{0,-1}]}---#{world[{1,-1}]}"
+      IO.puts "==============================="
+      tick(Enum.into(world, %{},
+        fn({pos,_}) -> 
+          {pos,(if lives?(world[pos],living_neighbors(world,pos)) do
+                :alive
+              else 
+                :dead
+              end)} 
+        end),generations-1)
+    end
+  end 
 end
--- test/game_of_life_test.exs
+++ test/game_of_life_test.exs
@@ -1,4 +1,305 @@
 defmodule GameOfLifeTest do
   use ExUnit.Case, async: true
   doctest GameOfLife
+
+  test "tick outputs next N generations" do 
+    world = %{{-1,1}  => :alive,  {0,1}  => :dead, {1,1}  => :alive,
+              {-1,0}  => :dead,   {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :alive,  {0,-1} => :dead, {1,-1} => :dead}
+    
+    next  = %{{-1,1}  => :alive,  {0,1}  => :alive, {1,1}  => :alive,
+              {-1,0}  => :alive,  {0,0}  => :alive, {1,0}  => :alive, 
+              {-1,-1} => :alive,  {0,-1} => :alive, {1,-1} => :alive}
+
+    last  = %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead}
+
+    blinker = %{{-1,1}  => :dead, {0,1}  => :alive, {1,1}  => :dead, 
+                {-1,0}  => :dead,  {0,0}  => :alive, {1,0}  => :dead, 
+                {-1,-1} => :dead,  {0,-1} => :alive, {1,-1} => :dead}
+    glider = %{{-1,1}  => :dead, {0,1}  => :alive, {1,1}  => :dead,
+               {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :alive,
+               {-1,-1} => :alive,  {0,-1} => :alive, {1,-1} => :alive}
+
+    assert GameOfLife.tick(world,1) == next
+    assert GameOfLife.tick(next,1) == last
+  end
+
+  test "Any live cell with fewer than two live neighbours dies, as if caused by underpopulation." do 
+    assert GameOfLife.lives?(:alive,1) == false
+    assert GameOfLife.lives?(:alive,0) == false
+  end
+
+  test "Any live cell with two or three live neighbours lives on to the next generation." do 
+    assert GameOfLife.lives?(:alive,2) == true
+    assert GameOfLife.lives?(:alive,3) == true
+  end
+
+  test "Any live cell with more than three live neighbours dies, as if by overpopulation." do 
+    assert GameOfLife.lives?(:alive,4) == false
+    assert GameOfLife.lives?(:alive,5) == false
+  end
+
+  test "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction." do 
+    assert GameOfLife.lives?(:dead,3) === true
+    assert GameOfLife.lives?(:dead,2) === false
+    assert GameOfLife.lives?(:dead,4) === false
+  end
+
+  defmodule LivingNeighborsTest do 
+    use ExUnit.Case, async: true
+    test "counts living neighbor to the North" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :alive, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :alive,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,1}) == 1
+    end
+
+    test "counts living neighbor to the South" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :alive,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :alive, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,-1}) == 1
+    end
+
+    test "counts living neighbor to the West" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :alive, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead,  {1,0}  => :alive, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,0}) == 1
+    end
+
+    test "counts living neighbor to the East" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :alive, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :alive, {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,0}) == 1
+    end
+
+    test "counts living neighbor to the NorthEast of center" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :alive,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+    end
+
+    test "counts living neighbor to the NorthEast of NE most cell" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :alive, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,1}) == 1
+    end
+
+    test "counts living neighbor to the NorthEast of northerly Cells" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :alive,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,1}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :alive
+              }
+      assert world |> GameOfLife.living_neighbors({0,1}) == 1
+    end
+
+    test "counts living neighbor to the NorthEast of easterly Cells" do 
+      world = 
+            %{{-1,1}  => :alive, {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,0}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead,  {1,1}  => :dead,
+              {-1,0}  => :alive, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthEast of center" do 
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :alive
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+    end
+
+    test "counts living neighbor to the SouthEast of SE most cell" do 
+      world = 
+            %{{-1,1}  => :alive, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthEast of southerly Cells" do   
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :alive, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,-1}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :alive,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthEast of easterly Cells" do   
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :alive, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,0}) == 1
+      world = 
+            %{{-1,1}  => :dead, {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :alive, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead, {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,1}) == 1
+    end
+
+    test "counts living neighbor to the NorthWest of center" do 
+      world = 
+            %{{-1,1}  => :alive,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+    end
+
+    test "counts living neighbor to the NorthWest of NW most cell" do   
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :alive
+              }
+      assert world |> GameOfLife.living_neighbors({-1,1}) == 1
+    end
+
+    test "counts living neighbor to the NorthWest of northerly cells" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :alive,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,1}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :alive,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,1}) == 1
+    end
+
+    test "counts living neighbor to the NorthWest of westerly cells" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :alive,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,0}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead, {0,0}  => :dead,  {1,0}  => :alive, 
+              {-1,-1} => :dead,  {0,-1} => :dead,  {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthWest of center" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :alive,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,0}) == 1
+    end
+
+    test "counts living neighbor to the SouthWest of SW most cell" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :alive,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthWest of southerly cells" do 
+      world = 
+            %{{-1,1}  => :alive,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({0,-1}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :alive, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({1,-1}) == 1
+    end
+
+    test "counts living neighbor to the SouthWest of westerly cells" do 
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :alive, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :dead
+              }
+      assert world |> GameOfLife.living_neighbors({-1,1}) == 1
+      world = 
+            %{{-1,1}  => :dead,  {0,1}  => :dead, {1,1}  => :dead,
+              {-1,0}  => :dead,  {0,0}  => :dead, {1,0}  => :dead, 
+              {-1,-1} => :dead,  {0,-1} => :dead, {1,-1} => :alive
+              }
+      assert world |> GameOfLife.living_neighbors({-1,0}) == 1
+    end
+  end
+
 end
