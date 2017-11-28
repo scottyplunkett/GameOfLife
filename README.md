6d64e34764150ffc7162aad676ab7fe0bcd84cd6
scottyplunkett <splunkett@8thLight.com>
2017-11-27 20:46:22 -0600

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # GameOfLife
 Conway's Game of Life in Elixir Implementation using TDD
+
+*Instructions assume you have Elixir installed*
+
+## Instructions
+To run:
+
+### 1
+clone repo to local machine
+```bash
+git clone https://github.com/scottyplunkett/GameOfLife.git
+```
+
+### 2
+change directory into local repo copy
+```bash
+cd GameOfLife
+```
+
+### 3
+compile app
+```bash
+mix compile 
+```
+
+### 4
+open interactive elixir shell with compiled app
+```bash
+iex -S mix 
+```
+
+### 5 
+have fun 
+```elixir
+almost_human = %{{-1,1}  => :alive, {0,1}  => :dead, {1,1}  => :alive,
+                {-1,0}  => :dead, {0,0}  => :alive, {1,0}  => :alive, 
+                {-1,-1} => :dead, {0,-1} => :dead, {1,-1} => :alive}
+
+GameOfLife.tick(almost_human,25)
+```
+
+## Running Test
+
+follow Instructions 1-3
+then: 
+```bash
+mix test --trace
+```
+
+## Generate Coverage Report
+
+follow Instructions 1-3
+then: 
+```bash
+mix test --cover
+```
+```bash
+cd cover
+```
+```bash
+open Elixir.GameOfLife.html
+```
+
--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -1,50 +1,43 @@
 defmodule GameOfLife do
-  def lives?(status,count) do 
-    if status == :alive do 
-      count >= 2 && count <= 3
-    else 
-      count === 3 
-    end
+
+  def iconify(cell) do if cell == :alive, do: " \u2619 ", else: " \u2620 " end
+
+  def grid(world) do 
+    (for {{_x,_y},status} <- Enum.sort(Map.to_list(world)), do: status) 
+    |> Enum.chunk_every(trunc(:math.sqrt(map_size(world)))) |> Enum.reverse
   end
 
-  def neighbors({col,row}) do 
-    for dx <- [-1, 0, 1], dy <- [-1, 0, 1], {dx,dy} != {0,0}, do: 
-      {col + dx, row + dy}
-  end
+  def lives?(:alive,count) do count >= 2 && count <= 3 end
+  def lives?(_,count)  do count === 3 end
 
   def living_neighbors(world,location) do
     Enum.count((for position <- neighbors(location), do: 
       world[position]), fn(status) -> status == :alive end)
   end
 
-  def new_world(old_world) do 
-    List.flatten(for {{x,y},_} <- old_world, do: neighbors({x,y}) 
-    |> List.flatten  
-    |> Enum.uniq)
-    |> Enum.into(%{},fn(pos) -> 
-      {pos,(if lives?(old_world[pos],living_neighbors(old_world,pos)) do
-              :alive
-            else 
-              :dead
-            end)}
-          end) 
+  def print(world) do 
+    IO.puts "\e[H\e[J"
+    for row <- grid(world), do: (for cell <- row, do: iconify(cell)) 
+    |> IO.puts 
+  end
+
+  def regenerate(world,pos) do 
+    if lives?(world[pos],living_neighbors(world,pos)), do: :alive, else: :dead
+  end
+
+  def neighbors({col,row},d\\[-1,0,1]) do 
+    for x <- d, y <- d, {x,y} != {0,0}, do: {col + x, row + y}
+  end
+
+  def next(world) do 
+    (for {{x,y},_} <- world, do: neighbors({x,y})) 
+    |> List.flatten |> Enum.uniq |> List.flatten
+    |> Enum.into(%{},fn(pos) -> {pos,regenerate(world, pos)} end) 
   end
 
   def tick(world,generations) do
     :timer.sleep 100
-    world |> print
-    IO.puts "\n"      
-    unless generations == 0 do 
-      tick(new_world(world),generations-1)
-    end
+    print(world)
+    unless generations == 0, do: tick(next(world),generations-1) 
   end 
-
-  def grid(world) do 
-    Enum.reverse(Enum.chunk_every((for {{_x,_y},status} <- Enum.sort(Map.to_list(world)), do: if status === :alive, do: " \u2619 ", else: " \u2620 "), trunc(:math.sqrt(map_size(world)))))
-  end
-
-  def print(world) do 
-    IO.puts "\e[H\e[J"
-    for row <- world |> grid, do: row |> IO.puts 
-  end
 end
